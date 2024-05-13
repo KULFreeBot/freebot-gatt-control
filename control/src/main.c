79df28af31d75df3e40d8a6f5bb0493f950f7476
Lowie Deferme <lowie.deferme@kuleuven.be>
2024-05-13 10:40:34 +0200

    init: controller stub

---
+++ control/src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Lowie Deferme <lowie.deferme@kuleuven.be>
+ * @brief BLE FreeBot controller
+ * @version 0.1
+ * @date 2024-05-10
+ *
+ * @copyright Copyright (c) 2024
+ */
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+    return 0;
+}
